5d3cee776201c5c68c261975feb26051588b3c35
iglesiju <32744974+iglesiju@users.noreply.github.com>
2017-11-01 20:11:32 -0400

    Form and Query Update
    Added more fields to the form and query to insert info into Student table using this info

--- RegisterAppointment.php
+++ RegisterAppointment.php
@@ -2,12 +2,28 @@
 
 include "dbconfig.php";
 
-$sname = $_POST["sname"];
+$sfname = $_POST["sfname"];
+$slname = $_POST["slname"];
 $sID = $_POST["sID"];
+$sEM = $_POST["sEM"];
 $reason = $_POST["reason"];
 $counselor = $_POST["counselor"];
 $location = $_POST["location"];
 $date = date("Y/m/d");
+
+$query1 = "INSERT into seds2017db.Students VALUES (\"$sID\",\"$sfname\",\"$slname\", null, null, null, \"$sEM\", null, null, null, \"teststudent\", \"jorgetriestohard\" )"
+
+$result1 = mysqli_query($conn, $query1);
+
+if($result)
+{
+
+}
+else
+{
+	echo "<BR><br>Error" . mysqli_error($conn);
+}
+
 
 $query = "INSERT into seds2017db.Students_Appointments VALUES (id, \"$sID\", \"$counselor\" , \"$location\" , \"$reason\", \"$date\" , 1 , 0 , 0 , null , null )";
 
@@ -16,7 +32,7 @@ $query = "INSERT into seds2017db.Students_Appointments VALUES (id, \"$sID\", \"$
 
 				if($result)
 				{
-					echo "<BR>Input succesful<BR>";
+					echo "<BR>Your appointment has been set. Please check your email for a confirmation code<BR>";
 				}
 				else
 				{
